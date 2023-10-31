131fcd274d280f1a03da9b88cf840ca63b0937ea
Douglas Nyberg <douglas.nyberg@tele2.com>
2023-10-31 16:10:07 +0100

    added working temperature widget

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,21 @@
 const clock = document.getElementById("clock");
 const day = document.getElementById("day");
 const dateMonthYear = document.getElementById("dateMonthYear");
+const temp = document.getElementById("temp");
 
-// function that updates the time every second
+// get weather status
+updateWeather();
+
+// function that updates the time & date every second
 setInterval(() => {
+    dates = getDates();
+    clock.innerHTML = 
+    ("0" + dates[5]).substr(-2) + ":" + ("0" + dates[4]).substr(-2);
+    day.innerHTML = translateDay(dates[3]);
+    dateMonthYear.innerHTML = `${dates[0]} ${convertMonth(dates[1])} ${dates[2]}`; 
+}, 1000)
+
+function getDates() {
     let d = new Date();
     let date = d.getDate();
     let month = d.getMonth();
@@ -14,11 +26,9 @@ setInterval(() => {
     let da = d.getDay();
     let m = d.getMinutes();
     let h = d.getHours();
-    clock.innerHTML = 
-    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2);
-    day.innerHTML = translateDay(da);
-    
-}, 10b 0)
+    let dates = [date, month, year, da, m, h];
+    return dates;
+}
 
 function translateDay(day) {
     switch (day) {
@@ -37,4 +47,46 @@ function translateDay(day) {
         case 0:
             return "SÖNDAG";
     }
+}
+
+function convertMonth(month) {
+    switch (month) {
+        case 0:
+            return "januari";
+        case 1:
+            return "februari";
+        case 2:
+            return "mars";
+        case 3:
+            return "april";
+        case 4:
+            return "maj";
+        case 5:
+            return "juni";
+        case 6:
+            return "juli";
+        case 7:
+            return "augusti";
+        case 8:
+            return "september";
+        case 9:
+            return "oktober";
+        case 10:
+            return "november";
+        case 11:
+            return "december";
+    }
+}
+
+
+async function getWeather() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=59.40316&lon=17.94479&appid=6c54c157dd0a9f4ebb1b0909374b6133&units=metric`)
+    const weather = await response.json();
+    return weather;
+} 
+
+async function updateWeather() {
+    const data = await getWeather();
+    const dataTemp = data.main.temp;
+    temp.innerHTML = `${Math.round(dataTemp)}°`;
 }
